bd096240bac2d95918acedac5f124124abbb0553
Mikołaj Daraż <mikolajdaraz@gmail.com>
2023-03-23 17:02:13 +0100

    Changed dir structure, added ex3

---
+++ ex3/src/main/kotlin/pl/edu/uj/projobj/ProjObjApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.uj.projobj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjObjApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjObjApplication>(*args)
+}
---
+++ ex3/src/test/kotlin/pl/edu/uj/projobj/ProjObjApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.uj.projobj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjObjApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
