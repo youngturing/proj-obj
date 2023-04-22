17471ac2dac40847031bc13933dec9f9bd17c188
youngturing <mikolajdaraz@gmail.com>
2023-04-22 16:27:38 +0200

    Added ex4

---
+++ ex42/cmd/api/handlers/posts_handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/akhil/go-echo-yt/cmd/api/service"
+	"github.com/labstack/echo"
+)
+
+func PostIndexHandler(c echo.Context) error {
+	data, err := service.GetAll()
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process the request")
+	}
+
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
+
+func PostSingleHandler(c echo.Context) error {
+	id := c.Param("id")
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process the request")
+	}
+	data, err := service.GetById(idx)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process the request")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
