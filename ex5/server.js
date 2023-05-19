0ae09616b64443db44be8ef2740e7f92bad53a0a
youngturing <mikolajdaraz@gmail.com>
2023-05-19 22:20:26 +0200

    Small corrections

--- ex5/server.js
+++ ex5/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Endpoint do pobierania danych o produktach
+
 app.get('/api/products', (req, res) => {
   const products = [
     { id: 1, name: 'Pen', price: 10 },
@@ -15,13 +15,12 @@ app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-// Endpoint do obsługi płatności
 app.post('/api/payments', (req, res) => {
   console.log('Received payement data:', req.body);
   res.sendStatus(200);
 });
 
-const port = 3001; // Wybierz odpowiedni numer portu
+const port = 3001; 
 app.listen(port, () => {
   console.log(`Servers' port ${port}`);
 });
